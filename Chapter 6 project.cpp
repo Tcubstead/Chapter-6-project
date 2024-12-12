2cfb322a41823d8c5046603399b87535d0762261
Tcubstead <tec44912@email.vccs.edu>
2024-12-12 15:07:33 -0500

    created user input and display for results

--- Chapter 6 project.cpp
+++ Chapter 6 project.cpp
@@ -6,10 +6,47 @@
 
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//calculations for perimeter and area
+double perimeterCalc(double length, double width) {
+    return 2 * (length + width);
+}
+
+double areaCalc(double length, double width) {
+    return length * width;
+}
+
+//repeating message that displays the [erimeter and area
+void displayRectangle(double perimeter, double area) {
+    cout << "The perimeter of the rectangle is: " << perimeter << endl;
+    cout << "The area of the rectangle is:" << area << endl;
+}
+int main() {
+    //variables for user input
+    double length, width, area, perimeter;
+    char repeat;
+
+    //user input to get length and width
+    do {
+        cout << "enter the length of the rectangle" << endl;
+        cin >> length;
+        cout << "enter the width of the rectangle" << endl;
+        cin >> width;
+
+        //calculations
+        perimeter = perimeterCalc(length, width);
+        area = areaCalc(length, width);
+
+        //displays results
+        displayRectangle(perimeter, area);
+
+        //loops program until the user ends it
+        cout << "Would you like to enter another rectangle (y/n)" << endl;
+        cin >> repeat;
+    } while (repeat == 'y' || repeat == 'Y');
+    cout << "Ending program" << endl;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
